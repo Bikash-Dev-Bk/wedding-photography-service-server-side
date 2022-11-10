17dab0931463815f48bc85cecce16f232f82bb03
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-11 03:13:19 +0600

    get services by id from db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 
 const app = express()
@@ -26,7 +26,14 @@ async function run() {
           res.send(services);
       });
 
-      
+      app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        console.log(service)
+        res.send(service);
+    });
+
   }
 
   finally {
