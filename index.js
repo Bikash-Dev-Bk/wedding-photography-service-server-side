fd4b66253cd5a6abf8b4a106a649a0cb7e9b3e50
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-10 22:36:46 +0600

    data create in bd and loaded from db to backend

--- index.js
+++ index.js
@@ -15,11 +15,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+      const serviceCollection = client.db('weddingPhotography').collection('services');
+       
+      app.get('/services', async (req, res) => {
+          const query = {}
+          const cursor = serviceCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+      });
+
+      
+  }
+
+  finally {
+
+  }
+}
+
+run().catch((err) => console.error(err));
+
 
 
 app.get('/', (req, res) => {
