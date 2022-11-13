87967308a7dde5d431d8ddc9e3791b1507d29dc7
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-13 14:59:46 +0600

    delete method added

--- index.js
+++ index.js
@@ -42,23 +42,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
-      console.log(service);
       res.send(service);
-    });
-
-    app.get("/reviews", async (req, res) => {
-      const query = {};
-      const cursor = reviewCollection.find(query);
-      const reviews = await cursor.toArray();
-      res.send(reviews);
-    });
-
-    app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewCollection.insertOne(review);
-      console.log(result);
-      review._id = result.insertedId;
-      res.send(review);
     });
 
     app.get("/team", async (req, res) => {
@@ -74,7 +58,38 @@ async function run() {
       const photoGallery = await cursor.toArray();
       res.send(photoGallery);
     });
+
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.get('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const user = await reviewCollection.findOne(query);
+      res.send(user);
+  })
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      review._id = result.insertedId;
+      res.send(review);
+    });
+
+    app.delete('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+
+  });
+
   } 
+
   finally {
 
   }
