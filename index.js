1333f23ad764c04b21485556f6087d6057072882
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-13 17:54:14 +0600

    update method added

--- index.js
+++ index.js
@@ -80,6 +80,20 @@ async function run() {
       res.send(review);
     });
 
+    app.put('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const review = req.body;
+      const option = {upsert: true};
+      const updatedReview = {
+          $set: {
+            myReview: review.myReview,
+          }
+      }
+      const result = await reviewCollection.updateOne(filter, updatedReview, option);
+      res.send(result);
+  })
+
     app.delete('/reviews/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) }
